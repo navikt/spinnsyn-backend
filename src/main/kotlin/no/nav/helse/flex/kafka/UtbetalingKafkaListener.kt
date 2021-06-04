09fe57b4a465954773222008e77af5ab601f7895
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-06-04 10:50:42 +0200

    Søk til rett før vi stoppet helse sporbar

--- src/main/kotlin/no/nav/helse/flex/kafka/UtbetalingKafkaListener.kt
+++ src/main/kotlin/no/nav/helse/flex/kafka/UtbetalingKafkaListener.kt
@@ -1,20 +1,25 @@
 package no.nav.helse.flex.kafka
 
 import no.nav.helse.flex.kafka.ConsumerStoppedEventExt.restart
+import no.nav.helse.flex.logger
 import no.nav.helse.flex.service.MottaUtbetalingService
 import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.TopicPartition
 import org.springframework.context.event.EventListener
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.event.ConsumerStoppedEvent
+import org.springframework.kafka.listener.ConsumerSeekAware
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
 
 const val UTBETALING_TOPIC = "tbd.utbetaling"
 
 @Component
 class UtbetalingKafkaListener(
     private val mottaUtbetalingService: MottaUtbetalingService
-) {
+) : ConsumerSeekAware {
     @KafkaListener(
         topics = [UTBETALING_TOPIC],
         containerFactory = "aivenKafkaListenerContainerFactory"
@@ -25,8 +30,21 @@ class UtbetalingKafkaListener(
         acknowledgment.acknowledge()
     }
 
+    val log = logger()
+
     @EventListener
     fun eventHandler(event: ConsumerStoppedEvent) {
         event.restart()
+    }
+
+    override fun onPartitionsAssigned(
+        assignments: Map<TopicPartition, Long>,
+        callback: ConsumerSeekAware.ConsumerSeekCallback
+    ) {
+        assignments.forEach { partition ->
+            val timestamp = OffsetDateTime.of(2021, 6, 3, 5, 35, 0, 0, ZoneOffset.UTC)
+            log.info("Søker til $timestamp for topic ${partition.key.topic()} partition: ${partition.key.partition()}")
+            callback.seekToTimestamp(partition.key.topic(), partition.key.partition(), timestamp.toInstant().toEpochMilli())
+        }
     }
 }
