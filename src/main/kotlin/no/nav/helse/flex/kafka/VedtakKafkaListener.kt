1832aac15d1c3908741ed3329bda4cb70a27e1e2
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-06-04 12:26:15 +0200

    Fjern duplikatsjekk og replay

--- src/main/kotlin/no/nav/helse/flex/kafka/VedtakKafkaListener.kt
+++ src/main/kotlin/no/nav/helse/flex/kafka/VedtakKafkaListener.kt
@@ -4,22 +4,18 @@ import no.nav.helse.flex.kafka.ConsumerStoppedEventExt.restart
 import no.nav.helse.flex.logger
 import no.nav.helse.flex.service.MottaVedtakService
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.apache.kafka.common.TopicPartition
 import org.springframework.context.event.EventListener
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.event.ConsumerStoppedEvent
-import org.springframework.kafka.listener.ConsumerSeekAware
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
-import java.time.OffsetDateTime
-import java.time.ZoneOffset
 
 const val VEDTAK_TOPIC = "tbd.vedtak"
 
 @Component
 class VedtakKafkaListener(
     private val mottaVedtakService: MottaVedtakService
-) : ConsumerSeekAware {
+) {
 
     val log = logger()
 
@@ -36,16 +32,5 @@ class VedtakKafkaListener(
     @EventListener
     fun eventHandler(event: ConsumerStoppedEvent) {
         event.restart()
-    }
-
-    override fun onPartitionsAssigned(
-        assignments: Map<TopicPartition, Long>,
-        callback: ConsumerSeekAware.ConsumerSeekCallback
-    ) {
-        assignments.forEach { partition ->
-            val timestamp = OffsetDateTime.of(2021, 6, 3, 5, 35, 0, 0, ZoneOffset.UTC)
-            log.info("Søker til $timestamp for topic ${partition.key.topic()} partition: ${partition.key.partition()}")
-            callback.seekToTimestamp(partition.key.topic(), partition.key.partition(), timestamp.toInstant().toEpochMilli())
-        }
     }
 }
--- src/main/kotlin/no/nav/helse/flex/service/MottaVedtakService.kt
+++ src/main/kotlin/no/nav/helse/flex/service/MottaVedtakService.kt
@@ -2,19 +2,15 @@ package no.nav.helse.flex.service
 
 import no.nav.helse.flex.db.VedtakDbRecord
 import no.nav.helse.flex.db.VedtakRepository
-import no.nav.helse.flex.domene.Dokument
-import no.nav.helse.flex.domene.VedtakFattetForEksternDto
 import no.nav.helse.flex.domene.tilVedtakFattetForEksternDto
 import no.nav.helse.flex.logger
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.stereotype.Service
 import java.time.Instant
-import java.time.LocalDate
 
 @Service
 class MottaVedtakService(
     private val vedtakRepository: VedtakRepository,
-    private val retroVedtakService: RetroVedtakService,
 ) {
     val log = logger()
 
@@ -31,15 +27,6 @@ class MottaVedtakService(
             vedtak.tilVedtakFattetForEksternDto()
         } catch (e: Exception) {
             throw RuntimeException("Kunne ikke deserialisere vedtak", e)
-        }
-
-        if (timestamp.isBefore(Instant.now().minusSeconds(120))) {
-            val gamleVedtak = retroVedtakService.hentRetroVedtak(fnr)
-            val duplikater = gamleVedtak.filter { it.matcher(vedtakSerialisert) }
-            if (duplikater.isNotEmpty()) {
-                log.info("Fant duplikat for vedtak med utbetalingsid ${vedtakSerialisert.utbetalingId} i gamle tabeller")
-                return
-            }
         }
 
         if (vedtakSerialisert.utbetalingId != null) {
@@ -68,32 +55,3 @@ class MottaVedtakService(
         log.info("Opprettet vedtak med database id: ${vedtakDB.id} for utbetaling id ${vedtakDB.utbetalingId}")
     }
 }
-
-private fun RetroRSVedtak.matcher(vedtakSerialisert: VedtakFattetForEksternDto): Boolean {
-    return this.tilSammenlikner() == vedtakSerialisert.tilSammenlikner()
-}
-
-private fun VedtakFattetForEksternDto.tilSammenlikner(): Sammenlikner {
-    return Sammenlikner(
-        fom = this.fom,
-        tom = this.tom,
-        dokumenter = this.dokumenter,
-        orgnr = this.organisasjonsnummer
-    )
-}
-
-private fun RetroRSVedtak.tilSammenlikner(): Sammenlikner {
-    return Sammenlikner(
-        fom = this.vedtak.fom,
-        tom = this.vedtak.tom,
-        dokumenter = this.vedtak.dokumenter,
-        orgnr = this.vedtak.organisasjonsnummer
-    )
-}
-
-data class Sammenlikner(
-    val fom: LocalDate,
-    val tom: LocalDate,
-    val dokumenter: List<Dokument>,
-    val orgnr: String?,
-)
