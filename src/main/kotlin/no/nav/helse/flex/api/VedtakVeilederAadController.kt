747640d899a2b91ba3042e6f217219b0b852ab28
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-08-30 14:31:28 +0200

    Returnerer data på nytt format til modia

--- src/main/kotlin/no/nav/helse/flex/api/VedtakVeilederAadController.kt
+++ src/main/kotlin/no/nav/helse/flex/api/VedtakVeilederAadController.kt
@@ -3,6 +3,7 @@ package no.nav.helse.flex.api
 import no.nav.helse.flex.client.SyfoTilgangskontrollOboClient
 import no.nav.helse.flex.clientidvalidation.ClientIdValidation
 import no.nav.helse.flex.clientidvalidation.ClientIdValidation.NamespaceAndApp
+import no.nav.helse.flex.domene.RSVedtakWrapper
 import no.nav.helse.flex.service.RetroRSVedtak
 import no.nav.helse.flex.service.VedtakService
 import no.nav.security.token.support.core.api.ProtectedWithClaims
@@ -12,17 +13,16 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 
 @Controller
-@RequestMapping("/api/v2/veileder")
 class VedtakVeilederAadController(
     private val clientIdValidation: ClientIdValidation,
     private val vedtakService: VedtakService,
     private val syfoTilgangskontrollClient: SyfoTilgangskontrollOboClient,
 ) {
 
-    @GetMapping("/vedtak", produces = [MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping("/api/v2/veileder/vedtak", produces = [MediaType.APPLICATION_JSON_VALUE])
     @ResponseBody
     @ProtectedWithClaims(issuer = "azureator")
-    fun hentVedtak(@RequestParam fnr: String): List<RetroRSVedtak> {
+    fun hentRetroVedtak(@RequestParam fnr: String): List<RetroRSVedtak> {
         clientIdValidation.validateClientId(
             NamespaceAndApp(
                 namespace = "teamsykefravr",
@@ -34,6 +34,23 @@ class VedtakVeilederAadController(
             throw IkkeTilgangException()
         }
         return vedtakService.hentRetroVedtak(fnr)
+    }
+
+    @GetMapping("/api/v3/veileder/vedtak", produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ResponseBody
+    @ProtectedWithClaims(issuer = "azureator")
+    fun hentVedtak(@RequestParam fnr: String): List<RSVedtakWrapper> {
+        clientIdValidation.validateClientId(
+            NamespaceAndApp(
+                namespace = "teamsykefravr",
+                app = "syfomodiaperson"
+            )
+        )
+
+        if (!syfoTilgangskontrollClient.sjekkTilgangVeileder(fnr)) {
+            throw IkkeTilgangException()
+        }
+        return vedtakService.hentVedtak(fnr)
     }
 }
 
