8c0d63b0bd2b2ad18c3323bd71b2b0d6f5a1b429
kulsveen <bjorn.inge.kulsveen@nav.no>
2021-09-17 09:14:56 +0200

    Revert "Setter offset til starten av dagen (#223)"
    This reverts commit 6ab83df8

--- src/main/kotlin/no/nav/helse/flex/kafka/HelseSporbarKafkaListener.kt
+++ src/main/kotlin/no/nav/helse/flex/kafka/HelseSporbarKafkaListener.kt
@@ -1,46 +1,25 @@
 package no.nav.helse.flex.kafka
 
-import no.nav.helse.flex.logger
 import no.nav.helse.flex.service.RetroMottaVedtakService
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.apache.kafka.common.TopicPartition
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.kafka.listener.ConsumerSeekAware
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
-import java.time.LocalDate
-import java.time.ZoneOffset
 
 const val SPORBAR_TOPIC = "aapen-helse-sporbar"
 
 @Component
 class HelseSporbarKafkaListener(
     private val retroMottaVedtakService: RetroMottaVedtakService
-) : ConsumerSeekAware {
-    private val log = logger()
+) {
 
     @KafkaListener(
         topics = [SPORBAR_TOPIC],
-        properties = ["auto.offset.reset = latest"],
-        containerFactory = "onPremKafkaListenerContainerFactory",
+        containerFactory = "onPremKafkaListenerContainerFactory"
     )
     fun listen(cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
         retroMottaVedtakService.handterMelding(cr)
 
         acknowledgment.acknowledge()
-    }
-
-    override fun registerSeekCallback(callback: ConsumerSeekAware.ConsumerSeekCallback) {
-        // register custom callback
-        log.info("registerSeekCallback ${this.javaClass.simpleName}")
-    }
-
-    override fun onPartitionsAssigned(assignments: Map<TopicPartition, Long>, callback: ConsumerSeekAware.ConsumerSeekCallback) {
-        // Seek all the assigned partition to a certain offset
-        log.info("onPartitionsAssigned ${this.javaClass.simpleName}")
-
-        val fredag17sept = LocalDate.of(2021, 9, 17).atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli()
-        callback.seekToTimestamp(assignments.keys, fredag17sept)
-        log.info("ferdig med seek seekToTimestamp ${this.javaClass.simpleName}")
     }
 }
