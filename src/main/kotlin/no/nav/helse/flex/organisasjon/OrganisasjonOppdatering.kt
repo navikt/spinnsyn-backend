fd1602afcbbed288130bced7c8a096b147ee0cde
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-10-26 15:46:57 +0200

    Lagrer orgnummer og navn i databasen

---
+++ src/main/kotlin/no/nav/helse/flex/organisasjon/OrganisasjonOppdatering.kt
@@ -0,0 +1,49 @@
+package no.nav.helse.flex.organisasjon
+
+import no.nav.helse.flex.logger
+import no.nav.syfo.kafka.felles.SykepengesoknadDTO
+import org.springframework.stereotype.Service
+import java.time.Instant
+
+@Service
+class OrganisasjonOppdatering(
+    private val organisasjonRepository: OrganisasjonRepository
+) {
+    val log = logger()
+
+    fun handterSoknad(soknad: SykepengesoknadDTO) {
+        if (soknad.harArbeidsgiver()) {
+            val orgnummer = soknad.arbeidsgiver!!.orgnummer!!
+            val navn = soknad.arbeidsgiver!!.navn!!
+            val eksisterende = organisasjonRepository.findByOrgnummer(orgnummer)
+
+            if (eksisterende == null) {
+                organisasjonRepository.save(
+                    Organisasjon(
+                        orgnummer = orgnummer,
+                        navn = navn,
+                        oppdatert = Instant.now(),
+                        opprettet = Instant.now(),
+                        oppdatertAv = soknad.id,
+                    )
+                )
+                return
+            } else {
+                if (eksisterende.navn != navn) {
+                    log.info("Endrer navn på $orgnummer fra ${eksisterende.navn} til $navn. Opprinnelig kilde ${eksisterende.oppdatertAv}. Ny kilde ${soknad.id}")
+                    organisasjonRepository.save(
+                        eksisterende.copy(
+                            navn = navn,
+                            oppdatertAv = soknad.id,
+                            oppdatert = Instant.now(),
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
+
+private fun SykepengesoknadDTO.harArbeidsgiver(): Boolean {
+    return this.arbeidsgiver?.orgnummer?.isNotBlank() ?: false && this.arbeidsgiver?.navn?.isNotBlank() ?: false
+}
---
+++ src/test/kotlin/no/nav/helse/flex/OppdaterOrganisasjonTabellTest.kt
@@ -0,0 +1,104 @@
+package no.nav.helse.flex
+
+import no.nav.helse.flex.kafka.FLEX_SYKEPENGESOKNAD_TOPIC
+import no.nav.syfo.kafka.felles.*
+import org.amshove.kluent.`should be equal to`
+import org.amshove.kluent.shouldBeNull
+import org.apache.kafka.clients.producer.Producer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.awaitility.Awaitility.await
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import java.time.LocalDate
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+class OppdaterOrganisasjonTabellTest : AbstractContainerBaseTest() {
+
+    @Autowired
+    lateinit var kafkaProducer: Producer<String, String>
+
+    @Test
+    fun `Oppretter ny organisasjon hvis den ikke finnes fra før`() {
+        organisasjonRepository.deleteAll()
+
+        val soknad = SykepengesoknadDTO(
+            fnr = "bla",
+            id = UUID.randomUUID().toString(),
+            type = SoknadstypeDTO.ARBEIDSTAKERE,
+            status = SoknadsstatusDTO.NY,
+            fom = LocalDate.now().minusDays(1),
+            tom = LocalDate.now(),
+            arbeidssituasjon = ArbeidssituasjonDTO.ARBEIDSTAKER,
+            arbeidsgiver = ArbeidsgiverDTO(navn = "Bedriften AS", orgnummer = "123456547")
+        )
+
+        organisasjonRepository.findByOrgnummer(soknad.arbeidsgiver!!.orgnummer!!).shouldBeNull()
+
+        sendSykepengesoknad(soknad)
+
+        await().atMost(10, TimeUnit.SECONDS).until {
+            organisasjonRepository.findByOrgnummer(soknad.arbeidsgiver!!.orgnummer!!) != null
+        }
+
+        val org = organisasjonRepository.findByOrgnummer(soknad.arbeidsgiver!!.orgnummer!!)!!
+        org.orgnummer `should be equal to` soknad.arbeidsgiver!!.orgnummer!!
+        org.navn `should be equal to` soknad.arbeidsgiver!!.navn!!
+        org.oppdatertAv `should be equal to` soknad.id
+    }
+
+    @Test
+    fun `Oppdaterer organisasjon hvis den finnes fra før`() {
+        organisasjonRepository.deleteAll()
+
+        val soknad = SykepengesoknadDTO(
+            fnr = "bla",
+            id = UUID.randomUUID().toString(),
+            type = SoknadstypeDTO.ARBEIDSTAKERE,
+            status = SoknadsstatusDTO.NY,
+            fom = LocalDate.now().minusDays(1),
+            tom = LocalDate.now(),
+            arbeidssituasjon = ArbeidssituasjonDTO.ARBEIDSTAKER,
+            arbeidsgiver = ArbeidsgiverDTO(navn = "Bedriften AS", orgnummer = "1234534")
+        )
+
+        organisasjonRepository.findByOrgnummer(soknad.arbeidsgiver!!.orgnummer!!).shouldBeNull()
+
+        sendSykepengesoknad(soknad)
+
+        await().atMost(10, TimeUnit.SECONDS).until {
+            organisasjonRepository.findByOrgnummer(soknad.arbeidsgiver!!.orgnummer!!) != null
+        }
+
+        val org = organisasjonRepository.findByOrgnummer(soknad.arbeidsgiver!!.orgnummer!!)!!
+        org.navn `should be equal to` soknad.arbeidsgiver!!.navn!!
+
+        val soknad2 = soknad.copy(
+            id = UUID.randomUUID().toString(),
+            arbeidsgiver = ArbeidsgiverDTO(
+                navn = "Bedriften AS Med nytt navn :)",
+                orgnummer = "1234534"
+            )
+        )
+        sendSykepengesoknad(soknad2)
+
+        await().atMost(10, TimeUnit.SECONDS).until {
+            organisasjonRepository.findByOrgnummer(soknad.arbeidsgiver!!.orgnummer!!)!!.navn == "Bedriften AS Med nytt navn :)"
+        }
+
+        val orgEtterOppdatering = organisasjonRepository.findByOrgnummer(soknad.arbeidsgiver!!.orgnummer!!)!!
+        orgEtterOppdatering.navn `should be equal to` "Bedriften AS Med nytt navn :)"
+        orgEtterOppdatering.oppdatertAv `should be equal to` soknad2.id
+    }
+
+    fun sendSykepengesoknad(soknad: SykepengesoknadDTO) {
+        kafkaProducer.send(
+            ProducerRecord(
+                FLEX_SYKEPENGESOKNAD_TOPIC,
+                null,
+                soknad.id,
+                soknad.serialisertTilString()
+            )
+        ).get()
+    }
+}
