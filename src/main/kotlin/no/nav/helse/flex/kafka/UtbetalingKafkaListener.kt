33c6332de6f04a749505f42d1d520d6f1fe32571
kulsveen <bjorn.inge.kulsveen@nav.no>
2021-11-05 10:40:05 +0100

    Setter offset på starten av dagen den 4 november

--- src/main/kotlin/no/nav/helse/flex/kafka/UtbetalingKafkaListener.kt
+++ src/main/kotlin/no/nav/helse/flex/kafka/UtbetalingKafkaListener.kt
@@ -4,21 +4,26 @@ import no.nav.helse.flex.kafka.ConsumerStoppedEventExt.restart
 import no.nav.helse.flex.logger
 import no.nav.helse.flex.service.MottaUtbetalingService
 import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.TopicPartition
 import org.springframework.context.event.EventListener
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.event.ConsumerStoppedEvent
+import org.springframework.kafka.listener.ConsumerSeekAware
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
+import java.time.LocalDate
+import java.time.ZoneOffset
 
 const val UTBETALING_TOPIC = "tbd.utbetaling"
 
 @Component
 class UtbetalingKafkaListener(
     private val mottaUtbetalingService: MottaUtbetalingService
-) {
+) : ConsumerSeekAware {
     @KafkaListener(
         topics = [UTBETALING_TOPIC],
-        containerFactory = "aivenKafkaListenerContainerFactory"
+        containerFactory = "aivenKafkaListenerContainerFactory",
+        properties = ["auto.offset.reset = latest"],
     )
     fun listen(cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
         mottaUtbetalingService.handterMelding(cr)
@@ -31,5 +36,19 @@ class UtbetalingKafkaListener(
     @EventListener
     fun eventHandler(event: ConsumerStoppedEvent) {
         event.restart()
+    }
+
+    override fun registerSeekCallback(callback: ConsumerSeekAware.ConsumerSeekCallback) {
+        // register custom callback
+        log.info("registerSeekCallback ${this.javaClass.simpleName}")
+    }
+
+    override fun onPartitionsAssigned(assignments: Map<TopicPartition, Long>, callback: ConsumerSeekAware.ConsumerSeekCallback) {
+        // Seek all the assigned partition to a certain offset
+        log.info("onPartitionsAssigned ${this.javaClass.simpleName}")
+
+        val torsdag4nov = LocalDate.of(2021, 11, 4).atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli()
+        callback.seekToTimestamp(assignments.keys, torsdag4nov)
+        log.info("ferdig med seek seekToTimestamp ${this.javaClass.simpleName}")
     }
 }
--- src/main/kotlin/no/nav/helse/flex/kafka/VedtakKafkaListener.kt
+++ src/main/kotlin/no/nav/helse/flex/kafka/VedtakKafkaListener.kt
@@ -4,24 +4,29 @@ import no.nav.helse.flex.kafka.ConsumerStoppedEventExt.restart
 import no.nav.helse.flex.logger
 import no.nav.helse.flex.service.MottaVedtakService
 import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.TopicPartition
 import org.springframework.context.event.EventListener
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.event.ConsumerStoppedEvent
+import org.springframework.kafka.listener.ConsumerSeekAware
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
+import java.time.LocalDate
+import java.time.ZoneOffset
 
 const val VEDTAK_TOPIC = "tbd.vedtak"
 
 @Component
 class VedtakKafkaListener(
     private val mottaVedtakService: MottaVedtakService
-) {
+) : ConsumerSeekAware {
 
     val log = logger()
 
     @KafkaListener(
         topics = [VEDTAK_TOPIC],
-        containerFactory = "aivenKafkaListenerContainerFactory"
+        containerFactory = "aivenKafkaListenerContainerFactory",
+        properties = ["auto.offset.reset = latest"],
     )
     fun listen(cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
         mottaVedtakService.handterMelding(cr)
@@ -32,5 +37,19 @@ class VedtakKafkaListener(
     @EventListener
     fun eventHandler(event: ConsumerStoppedEvent) {
         event.restart()
+    }
+
+    override fun registerSeekCallback(callback: ConsumerSeekAware.ConsumerSeekCallback) {
+        // register custom callback
+        log.info("registerSeekCallback ${this.javaClass.simpleName}")
+    }
+
+    override fun onPartitionsAssigned(assignments: Map<TopicPartition, Long>, callback: ConsumerSeekAware.ConsumerSeekCallback) {
+        // Seek all the assigned partition to a certain offset
+        log.info("onPartitionsAssigned ${this.javaClass.simpleName}")
+
+        val torsdag4nov = LocalDate.of(2021, 11, 4).atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli()
+        callback.seekToTimestamp(assignments.keys, torsdag4nov)
+        log.info("ferdig med seek seekToTimestamp ${this.javaClass.simpleName}")
     }
 }
