fc0af422d5b190f8a51631b1d55cfebd19df2e75
Bjørn Inge Kulsveen <35369790+kulsveen@users.noreply.github.com>
2021-11-12 10:27:10 +0100

    Slår på flyway (#266)

--- src/main/resources/db/migration/V25__setter_motatt_publisert.sql
+++
@@ -1,3 +0,0 @@
-UPDATE UTBETALING
-    SET motatt_publisert = CURRENT_TIMESTAMP
-    WHERE motatt_publisert IS NULL;
--- src/main/resources/db/migration/V26__index_utbetaling_motatt_publisert.sql
+++
@@ -1,1 +0,0 @@
-create index utbetaling_motatt_publisert_idx on utbetaling (motatt_publisert);
