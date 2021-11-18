604639b6c44bf41f068632649e16bc0c4c73c8e4
golshidbahadorian <golshid.bahadorian@nav.no>
2021-11-18 08:35:25 +0100

    Legger til test for erstatting av orgNr med orgNavn

---
+++ src/test/kotlin/no/nav/helse/flex/ErstattOrgNrMedOrgNavnTest.kt
@@ -0,0 +1,85 @@
+package no.nav.helse.flex
+
+import no.nav.helse.flex.domene.*
+import no.nav.helse.flex.organisasjon.LeggTilOrganisasjonnavn
+import no.nav.helse.flex.organisasjon.Organisasjon
+import org.amshove.kluent.`should be equal to`
+import org.apache.kafka.clients.producer.Producer
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import java.time.Instant
+import java.time.LocalDate
+
+class ErstattOrgNrMedOrgNavnTest : AbstractContainerBaseTest() {
+
+    @Autowired
+    lateinit var kafkaProducer: Producer<String, String>
+
+    @Autowired
+    lateinit var leggTilOrganisasjonnavn: LeggTilOrganisasjonnavn
+
+    @Test
+    fun `Erstatter orgNr med OrgNavn`() {
+        organisasjonRepository.deleteAll()
+
+        val vedtak = RSVedtakWrapper(
+            id = "1",
+            lest= false,
+            lestDato = null,
+            vedtak= RSVedtak(
+                organisasjonsnummer = "1234",
+                fom = LocalDate.now(),
+                tom = LocalDate.now(),
+                dokumenter = emptyList(),
+                utbetaling = RSUtbetalingUtbetalt(
+                    utbetalingId = null,
+                    organisasjonsnummer = "1234",
+                    forbrukteSykedager = 0,
+                    gjenståendeSykedager = 0,
+                    automatiskBehandling = true,
+                    utbetalingsdager = emptyList(),
+                    utbetalingType = "",
+                    personOppdag = null,
+                    arbeidsgiverOppdrag = null,
+                    foreløpigBeregnetSluttPåSykepenger = null
+                ),
+                grunnlagForSykepengegrunnlagPerArbeidsgiver = mapOf("123456547" to 500000.0, "547123456" to 300000.0, "5678" to 100000.0),
+                begrensning = null,
+                grunnlagForSykepengegrunnlag = null,
+                inntekt = null,
+                sykepengegrunnlag = null
+            ),
+            opprettet = LocalDate.now(),
+            opprettetTimestamp = Instant.now(),
+            orgnavn= "1234",
+            annullert= false,
+            revurdert= false,
+            dager= emptyList(),
+            dagerArbeidsgiver= emptyList(),
+            dagerPerson= emptyList(),
+            sykepengebelop= 0,
+            sykepengebelopArbeidsgiver= 0,
+            sykepengebelopPerson= 0,
+        )
+
+        val org1 = Organisasjon(
+            orgnummer = "123456547",
+            navn = "Organisasjon nr 1",
+            id = null,
+            oppdatert = Instant.now(),
+            oppdatertAv = "",
+            opprettet = Instant.now()
+        )
+
+        organisasjonRepository.saveAll(mutableListOf(org1))
+
+        val orgMap = leggTilOrganisasjonnavn.erstattOrgNummerMedOrgNavn(vedtakene = listOf(vedtak))
+        orgMap[0].vedtak.grunnlagForSykepengegrunnlagPerArbeidsgiver `should be equal to`
+            mapOf(
+                "Organisasjon nr 1" to 500000.0,
+                "Organisasjonsnummer: 547123456" to 300000.0,
+                "Organisasjonsnummer: 5678" to 100000.0
+            )
+    }
+
+}
