d0703670a04e51f971b22c99f70ae1228edb6100
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2021-11-29 09:35:42 +0100

    Legg til cutoff for brukernotifikasjon

--- src/main/kotlin/no/nav/helse/flex/cronjob/BrukernotifikasjonJob.kt
+++ src/main/kotlin/no/nav/helse/flex/cronjob/BrukernotifikasjonJob.kt
@@ -4,17 +4,29 @@ import no.nav.helse.flex.logger
 import no.nav.helse.flex.service.BrukernotifikasjonService
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 @Component
 class BrukernotifikasjonJob(
     val leaderElection: LeaderElection,
     val brukernotifikasjonService: BrukernotifikasjonService
 ) {
-    val log = logger()
+    private val log = logger()
+    private val sisteTidForBrukernotifikasjon = ZonedDateTime.of(LocalDate.of(2021, 11, 29).atTime(10, 0), ZoneId.of(ZONE_ID_OSLO))
 
     @Scheduled(cron = "0 0/10 * * * ?")
     fun run() {
+        val tid = ZonedDateTime.now(ZoneId.of(ZONE_ID_OSLO))
+
         if (leaderElection.isLeader()) {
+
+            // TODO: Fjern sammen med annen kode relatert til brukernotifikasjoner etter cutoff tidspunkt.
+            if (tid.isAfter(sisteTidForBrukernotifikasjon)) {
+                log.info("Kjører ikke brukernotifikasjonsjobb da klokken er [$tid], som er etter [$sisteTidForBrukernotifikasjon].")
+                return
+            }
 
             log.info("Kjører brukernotifikasjonjob")
             val antall = brukernotifikasjonService.prosseserUtbetaling()
@@ -24,3 +36,5 @@ class BrukernotifikasjonJob(
         }
     }
 }
+
+const val ZONE_ID_OSLO = "Europe/Oslo"
