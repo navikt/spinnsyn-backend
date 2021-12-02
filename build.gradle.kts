5bf225d647b117f1961958d9a13ca9c409226869
Nils Jørgen Mittet <88632815+nilsjorgen@users.noreply.github.com>
2021-12-02 12:29:15 +0100

    Dependabot updates (#287)
    - Bumps [brukernotifikasjon-schemas](https://github.com/navikt/brukernotifikasjon-schemas) from 1.2021.01.18-11.12-b9c8c40b98d1 to 2.3.1.
    - Bumps [kafka-avro-serializer](https://github.com/confluentinc/schema-registry) from 6.2.0 to 7.0.0.
    - Bumps [logstash-logback-encoder](https://github.com/logfellow/logstash-logback-encoder) from 6.6 to 7.0.1.
    - Bumps [actions/cache](https://github.com/actions/cache) from 2.1.6 to 2.1.7.
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,10 +44,10 @@ repositories {
 }
 
 val testContainersVersion = "1.16.2"
-val logstashLogbackEncoderVersion = "6.6"
+val logstashLogbackEncoderVersion = "7.0.1"
 val kluentVersion = "1.68"
-val brukernotifikasjonAvroVersion = "1.2021.01.18-11.12-b9c8c40b98d1"
-val confluentVersion = "6.2.0"
+val brukernotifikasjonAvroVersion = "2.3.1"
+val confluentVersion = "7.0.0"
 val tokenSupportVersion = "1.3.9"
 val syfoKafkaVersion = "2021.07.20-09.39-6be2c52c"
 
