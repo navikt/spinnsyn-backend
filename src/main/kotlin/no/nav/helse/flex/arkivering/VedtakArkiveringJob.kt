f00fac0da1eb964b874c5fcd4c4bc9436c8437fc
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2022-01-12 08:58:56 +0100

    Start arkivering av utbetalinger

--- src/main/kotlin/no/nav/helse/flex/arkivering/VedtakArkiveringJob.kt
+++ src/main/kotlin/no/nav/helse/flex/arkivering/VedtakArkiveringJob.kt
@@ -2,7 +2,9 @@ package no.nav.helse.flex.arkivering
 
 import no.nav.helse.flex.cronjob.LeaderElection
 import no.nav.helse.flex.logger
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
 
 const val ARKIVERING_BATCH_SIZE = 100
 
@@ -14,7 +16,7 @@ class VedtakArkiveringJob(
 
     val log = logger()
 
-    // @Scheduled(initialDelay = 120L, fixedDelay = 180L, timeUnit = TimeUnit.SECONDS)
+    @Scheduled(initialDelay = 120L, fixedDelay = 60L, timeUnit = TimeUnit.SECONDS)
     fun arkiverUtbetalinger() {
         if (leaderElection.isLeader()) {
             try {
