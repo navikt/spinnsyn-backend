2909bf46b516b00131f560d68b2d478a92719489
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2024-01-22 13:41:59 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ val testContainersVersion = "1.19.3"
 val logstashLogbackEncoderVersion = "7.4"
 val kluentVersion = "1.73"
 val tokenSupportVersion = "3.2.0"
-val sykepengesoknadKafkaVersion = "2023.12.05-10.16-3ffa06f7"
+val sykepengesoknadKafkaVersion = "2024.01.11-14.12-7adc3f4a"
 
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
