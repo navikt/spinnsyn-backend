9ddb8d2a682c402820b83d76a38c516e9e0463b5
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-08-15 10:16:19 +0200

    støtter svartype OPPSUMMERING

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ val testContainersVersion = "1.20.1"
 val logstashLogbackEncoderVersion = "8.0"
 val kluentVersion = "1.73"
 val tokenSupportVersion = "5.0.1"
-val sykepengesoknadKafkaVersion = "2024.08.07-13.06-828fc10c"
+val sykepengesoknadKafkaVersion = "2024.08.14-11.09-eefd3265"
 
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
