5c37f1ef5f401e067757c02da730871173740f83
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2026-01-28 11:25:31 +0100

    Refaktorerer hentRelaterteVedtak og mapTilRsVedtakWrapper til companion object

--- src/main/kotlin/no/nav/helse/flex/service/BrukerVedtak.kt
+++ src/main/kotlin/no/nav/helse/flex/service/BrukerVedtak.kt
@@ -2,27 +2,12 @@ package no.nav.helse.flex.service
 
 import no.nav.helse.flex.api.AbstractApiError
 import no.nav.helse.flex.api.LogLevel
-import no.nav.helse.flex.db.Annullering
-import no.nav.helse.flex.db.AnnulleringDAO
-import no.nav.helse.flex.db.UtbetalingDbRecord
-import no.nav.helse.flex.db.UtbetalingRepository
-import no.nav.helse.flex.db.VedtakDbRecord
-import no.nav.helse.flex.db.VedtakRepository
-import no.nav.helse.flex.domene.RSUtbetalingUtbetalt
-import no.nav.helse.flex.domene.RSVedtak
-import no.nav.helse.flex.domene.RSVedtakWrapper
-import no.nav.helse.flex.domene.VedtakStatus
-import no.nav.helse.flex.domene.VedtakStatusDTO
-import no.nav.helse.flex.domene.tilUtbetalingUtbetalt
-import no.nav.helse.flex.domene.tilVedtakFattetForEksternDto
+import no.nav.helse.flex.db.*
+import no.nav.helse.flex.domene.*
 import no.nav.helse.flex.kafka.VedtakStatusKafkaProducer
 import no.nav.helse.flex.logger
 import no.nav.helse.flex.organisasjon.LeggTilOrganisasjonnavn
-import no.nav.helse.flex.util.annullererVedtak
-import no.nav.helse.flex.util.leggTilDagerIVedtakPeriode
-import no.nav.helse.flex.util.markerRevurderte
-import no.nav.helse.flex.util.tilRsOppdrag
-import no.nav.helse.flex.util.tilRsUtbetalingsdag
+import no.nav.helse.flex.util.*
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import java.time.Instant
@@ -95,60 +80,70 @@ class BrukerVedtak(
         return "Leste vedtak $vedtaksId"
     }
 
-    internal fun mapTilRsVedtakWrapper(
-        utbetalingDbRecord: UtbetalingDbRecord,
-        vedtakMedUtbetaling: List<VedtakDbRecord>,
-        annulleringer: List<Annullering>,
-    ): RSVedtakWrapper {
-        val vedtakForUtbetaling =
-            hentRelaterteVedtak(
-                utbetalingDbRecord,
-                vedtakMedUtbetaling,
-            ).map { it.vedtak.tilVedtakFattetForEksternDto() }
-        val vedtaket = vedtakForUtbetaling.first()
-        val utbetalingen = utbetalingDbRecord.utbetaling.tilUtbetalingUtbetalt()
+    companion object {
+        internal fun hentRelaterteVedtak(
+            utbetalingDbRecord: UtbetalingDbRecord,
+            vedtakMedUtbetaling: List<VedtakDbRecord>,
+        ): List<VedtakDbRecord> =
+            vedtakMedUtbetaling
+                .filter { it.utbetalingId == utbetalingDbRecord.utbetalingId }
+                .sortedBy { it.id }
 
-        return RSVedtakWrapper(
-            id = utbetalingDbRecord.id!!,
-            annullert = annulleringer.annullererVedtak(vedtaket),
-            lest = utbetalingDbRecord.lest != null,
-            orgnavn = vedtaket.organisasjonsnummer,
-            andreArbeidsgivere = vedtaket.grunnlagForSykepengegrunnlagPerArbeidsgiver,
-            lestDato = utbetalingDbRecord.lest?.atZone(ZoneId.of("Europe/Oslo"))?.toOffsetDateTime(),
-            opprettetTimestamp = utbetalingDbRecord.opprettet,
-            vedtak =
-                RSVedtak(
-                    organisasjonsnummer = vedtaket.organisasjonsnummer,
-                    yrkesaktivitetstype = vedtaket.yrkesaktivitetstype ?: "ARBEIDSTAKER",
-                    dokumenter = vedtakForUtbetaling.flatMap { it.dokumenter },
-                    sykepengegrunnlag = vedtaket.sykepengegrunnlag,
-                    inntekt = vedtaket.inntekt,
-                    fom = vedtakForUtbetaling.minOf { it.fom },
-                    tom = vedtakForUtbetaling.maxOf { it.tom },
-                    grunnlagForSykepengegrunnlag = vedtaket.grunnlagForSykepengegrunnlag,
-                    grunnlagForSykepengegrunnlagPerArbeidsgiver = vedtaket.grunnlagForSykepengegrunnlagPerArbeidsgiver,
-                    begrensning = vedtaket.begrensning,
-                    vedtakFattetTidspunkt = vedtaket.vedtakFattetTidspunkt,
-                    sykepengegrunnlagsfakta = vedtaket.sykepengegrunnlagsfakta,
-                    begrunnelser = vedtaket.begrunnelser,
-                    tags = vedtaket.tags,
-                    utbetaling =
-                        RSUtbetalingUtbetalt(
-                            utbetalingType = utbetalingen.type,
-                            organisasjonsnummer = utbetalingen.organisasjonsnummer,
-                            forbrukteSykedager = utbetalingen.forbrukteSykedager,
-                            gjenståendeSykedager = utbetalingen.gjenståendeSykedager,
-                            foreløpigBeregnetSluttPåSykepenger = utbetalingen.foreløpigBeregnetSluttPåSykepenger,
-                            automatiskBehandling = utbetalingen.automatiskBehandling,
-                            utbetalingsdager = utbetalingen.utbetalingsdager.map { it.tilRsUtbetalingsdag() },
-                            utbetalingId = utbetalingen.utbetalingId,
-                            arbeidsgiverOppdrag = utbetalingen.arbeidsgiverOppdrag?.tilRsOppdrag(),
-                            personOppdrag = utbetalingen.personOppdrag?.tilRsOppdrag(),
-                        ),
-                    saksbehandler = vedtaket.saksbehandler,
-                    beslutter = vedtaket.beslutter,
-                ),
-        )
+        fun mapTilRsVedtakWrapper(
+            utbetalingDbRecord: UtbetalingDbRecord,
+            vedtakMedUtbetaling: List<VedtakDbRecord>,
+            annulleringer: List<Annullering>,
+        ): RSVedtakWrapper {
+            val vedtakForUtbetaling =
+                hentRelaterteVedtak(
+                    utbetalingDbRecord,
+                    vedtakMedUtbetaling,
+                ).map { it.vedtak.tilVedtakFattetForEksternDto() }
+            val vedtaket = vedtakForUtbetaling.first()
+            val utbetalingen = utbetalingDbRecord.utbetaling.tilUtbetalingUtbetalt()
+
+            return RSVedtakWrapper(
+                id = utbetalingDbRecord.id!!,
+                annullert = annulleringer.annullererVedtak(vedtaket),
+                lest = utbetalingDbRecord.lest != null,
+                orgnavn = vedtaket.organisasjonsnummer,
+                andreArbeidsgivere = vedtaket.grunnlagForSykepengegrunnlagPerArbeidsgiver,
+                lestDato = utbetalingDbRecord.lest?.atZone(ZoneId.of("Europe/Oslo"))?.toOffsetDateTime(),
+                opprettetTimestamp = utbetalingDbRecord.opprettet,
+                vedtak =
+                    RSVedtak(
+                        organisasjonsnummer = vedtaket.organisasjonsnummer,
+                        yrkesaktivitetstype = vedtaket.yrkesaktivitetstype ?: "ARBEIDSTAKER",
+                        dokumenter = vedtakForUtbetaling.flatMap { it.dokumenter },
+                        sykepengegrunnlag = vedtaket.sykepengegrunnlag,
+                        inntekt = vedtaket.inntekt,
+                        fom = vedtakForUtbetaling.minOf { it.fom },
+                        tom = vedtakForUtbetaling.maxOf { it.tom },
+                        grunnlagForSykepengegrunnlag = vedtaket.grunnlagForSykepengegrunnlag,
+                        grunnlagForSykepengegrunnlagPerArbeidsgiver = vedtaket.grunnlagForSykepengegrunnlagPerArbeidsgiver,
+                        begrensning = vedtaket.begrensning,
+                        vedtakFattetTidspunkt = vedtaket.vedtakFattetTidspunkt,
+                        sykepengegrunnlagsfakta = vedtaket.sykepengegrunnlagsfakta,
+                        begrunnelser = vedtaket.begrunnelser,
+                        tags = vedtaket.tags,
+                        utbetaling =
+                            RSUtbetalingUtbetalt(
+                                utbetalingType = utbetalingen.type,
+                                organisasjonsnummer = utbetalingen.organisasjonsnummer,
+                                forbrukteSykedager = utbetalingen.forbrukteSykedager,
+                                gjenståendeSykedager = utbetalingen.gjenståendeSykedager,
+                                foreløpigBeregnetSluttPåSykepenger = utbetalingen.foreløpigBeregnetSluttPåSykepenger,
+                                automatiskBehandling = utbetalingen.automatiskBehandling,
+                                utbetalingsdager = utbetalingen.utbetalingsdager.map { it.tilRsUtbetalingsdag() },
+                                utbetalingId = utbetalingen.utbetalingId,
+                                arbeidsgiverOppdrag = utbetalingen.arbeidsgiverOppdrag?.tilRsOppdrag(),
+                                personOppdrag = utbetalingen.personOppdrag?.tilRsOppdrag(),
+                            ),
+                        saksbehandler = vedtaket.saksbehandler,
+                        beslutter = vedtaket.beslutter,
+                    ),
+            )
+        }
     }
 
     internal fun harAlleVedtak(
@@ -158,14 +153,6 @@ class BrukerVedtak(
         vedtakMedUtbetaling
             .filter { it.utbetalingId == utbetalingDbRecord.utbetalingId }
             .size == utbetalingDbRecord.antallVedtak
-
-    internal fun hentRelaterteVedtak(
-        utbetalingDbRecord: UtbetalingDbRecord,
-        vedtakMedUtbetaling: List<VedtakDbRecord>,
-    ): List<VedtakDbRecord> =
-        vedtakMedUtbetaling
-            .filter { it.utbetalingId == utbetalingDbRecord.utbetalingId }
-            .sortedBy { it.id }
 
     private fun finnAlleVedtak(
         identer: List<String>,
