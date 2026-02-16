8b5ef5ce17527a7cbd75615f376faf173f849cd1
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2026-02-16 11:28:16 +0100

    Bytter localdate.now med hardkodet dato

--- src/test/kotlin/no/nav/helse/flex/service/FiltrerArbeidIkkeGjennopptattKtTest.kt
+++ src/test/kotlin/no/nav/helse/flex/service/FiltrerArbeidIkkeGjennopptattKtTest.kt
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test
 import java.time.LocalDate
 
 class FiltrerArbeidIkkeGjennopptattKtTest {
+    private val dato: LocalDate = LocalDate.parse("2021-01-01")
+
     @Test
     fun `fjerner arbeid ikke gjenopptatt dager`() {
         val vedtakMedDager =
@@ -19,12 +21,12 @@ class FiltrerArbeidIkkeGjennopptattKtTest {
                         rSDag(4, "ArbeidIkkeGjenopptattDag"),
                         rSDag(3, "NAVDag"),
                     ),
-                vedtak = vedtakTestdata().vedtak.copy(fom = LocalDate.now().minusDays(5)),
+                vedtak = vedtakTestdata().vedtak.copy(fom = dato.minusDays(5)),
             )
 
         val vedtakUtenArbeidIkkeGjenopptattDager = vedtakMedDager.fjernArbeidIkkeGjenopptattDager()
         vedtakUtenArbeidIkkeGjenopptattDager.dagerPerson.shouldHaveSize(1)
-        vedtakUtenArbeidIkkeGjenopptattDager.vedtak.fom shouldBeEqualTo (LocalDate.now().minusDays(3))
+        vedtakUtenArbeidIkkeGjenopptattDager.vedtak.fom shouldBeEqualTo (dato.minusDays(3))
     }
 
     @Test
@@ -37,12 +39,13 @@ class FiltrerArbeidIkkeGjennopptattKtTest {
                         rSDag(4, "NAVDag"),
                         rSDag(3, "NAVDag"),
                     ),
-                vedtak = vedtakTestdata().vedtak.copy(fom = LocalDate.now().minusDays(5)),
+                vedtak = vedtakTestdata().vedtak.copy(fom = dato.minusDays(5)),
             )
 
         val vedtakUtenArbeidIkkeGjenopptattDager = vedtakMedDager.fjernArbeidIkkeGjenopptattDager()
         vedtakUtenArbeidIkkeGjenopptattDager.dagerPerson.shouldHaveSize(3)
         vedtakUtenArbeidIkkeGjenopptattDager.vedtak.fom shouldBeEqualTo (LocalDate.now().minusDays(5))
+            vedtakUtenArbeidIkkeGjenopptattDager.dagerPerson.maxByOrNull { it.dato }!!.dato
     }
 
     fun rSDag(
@@ -50,7 +53,7 @@ class FiltrerArbeidIkkeGjennopptattKtTest {
         dagtype: String,
     ): RSDag =
         RSDag(
-            dato = LocalDate.now().minusDays(dagersiden.toLong()),
+            dato = dato.minusDays(dagersiden.toLong()),
             dagtype = dagtype,
             begrunnelser = emptyList(),
             belop = 12,
