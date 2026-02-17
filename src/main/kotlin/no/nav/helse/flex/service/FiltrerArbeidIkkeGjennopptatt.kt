6c0265d9f4dee3a8141b82dfb1a78a67a444a1a5
Steinar Sørli Hegna <steinar.sorli.hegna@nav.no>
2026-02-17 10:41:52 +0100

    Revert "fjernArbeidIkkeGjenopptattDager for utbetalingsdager"
    This reverts commit 2ae7dff0014b7f9d3dd7c7ce136b474dbba3b8da.

--- src/main/kotlin/no/nav/helse/flex/service/FiltrerArbeidIkkeGjennopptatt.kt
+++ src/main/kotlin/no/nav/helse/flex/service/FiltrerArbeidIkkeGjennopptatt.kt
@@ -3,44 +3,24 @@ package no.nav.helse.flex.service
 import no.nav.helse.flex.domene.RSVedtakWrapper
 import java.time.LocalDate
 
+// TODO: Konverter til å bruke utbetalingsdager
 fun RSVedtakWrapper.fjernArbeidIkkeGjenopptattDager(): RSVedtakWrapper {
     val dagerArbeidsgiver = this.dagerArbeidsgiver.filter { it.dagtype != "ArbeidIkkeGjenopptattDag" }
     val dagerPerson = this.dagerPerson.filter { it.dagtype != "ArbeidIkkeGjenopptattDag" }
 
-    val utbetalingsdager =
-        this.vedtak.utbetaling.utbetalingsdager
-            ?.filter { it.type != "ArbeidIkkeGjenopptattDag" } ?: emptyList()
-
     fun finnFaktiskFom(): LocalDate {
-        val tidligstedag = (dagerArbeidsgiver + dagerPerson).minByOrNull { it.dato }
-        return tidligstedag?.dato ?: this.vedtak.fom
-    }
-
-    fun finnFaktiskFomUtbetalingsdager(): LocalDate {
-        val tidligstedag = utbetalingsdager.minByOrNull { it.dato }
-        return tidligstedag?.dato ?: this.vedtak.fom
-    }
-
-    val faktiskFomUtbetalingsdager = finnFaktiskFomUtbetalingsdager()
-    val faktiskFom = finnFaktiskFom()
-
-    if (!faktiskFomUtbetalingsdager.equals(faktiskFom)) {
-        throw IllegalStateException(
-            "Faktisk fom fra utbetalingsdager er forskjellig fra faktisk fom fra dagerArbeidsgiver/dagerPerson. " +
-                "faktiskFomUtbetalingsdager=$faktiskFomUtbetalingsdager, faktiskFom=$faktiskFom",
-        )
+        val tidligsteDagerDag = (dagerArbeidsgiver + dagerPerson).minByOrNull { it.dato }
+        if (tidligsteDagerDag != null) {
+            if (tidligsteDagerDag.dato.isAfter(this.vedtak.fom)) {
+                return tidligsteDagerDag.dato
+            }
+        }
+        return this.vedtak.fom
     }
 
     return this.copy(
         dagerArbeidsgiver = dagerArbeidsgiver,
         dagerPerson = dagerPerson,
-        vedtak =
-            this.vedtak.copy(
-                fom = faktiskFomUtbetalingsdager,
-                utbetaling =
-                    this.vedtak.utbetaling.copy(
-                        utbetalingsdager = utbetalingsdager,
-                    ),
-            ),
+        vedtak = this.vedtak.copy(fom = finnFaktiskFom()),
     )
 }
